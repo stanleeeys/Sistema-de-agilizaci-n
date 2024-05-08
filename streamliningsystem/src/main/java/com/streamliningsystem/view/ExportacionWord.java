82a564f0e66662d8cba6f96b888941b19a989107
criquez1237 <ing.chenriquez@outlook.com>
2024-05-08 12:49:37 -0600

    feat: implement Word document generation for contracts or reports

--- streamliningsystem/src/main/java/com/streamliningsystem/view/ExportacionWord.java
+++ streamliningsystem/src/main/java/com/streamliningsystem/view/ExportacionWord.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.streamliningsystem.view;
 
 import com.streamliningsystem.model.Cotizacion;
@@ -11,66 +7,118 @@ import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 import org.apache.poi.xwpf.usermodel.XWPFTableCell;
-
+import org.apache.poi.xwpf.usermodel.*;
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import java.io.FileOutputStream;
 import java.util.List;
-import java.util.ArrayList;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.STBorder;
 
 public class ExportacionWord {
 
     public void exportarTabla(List<Cotizacion> listaCotizaciones, String nombreArchivo) {
         try {
-            // Crear documento .docx
             XWPFDocument documento = new XWPFDocument();
 
-            // Crear párrafo para título
+           
             XWPFParagraph titulo = documento.createParagraph();
+            titulo.setAlignment(ParagraphAlignment.CENTER);
+            titulo.setSpacingAfter(200); 
             XWPFRun tituloRun = titulo.createRun();
-            tituloRun.setText("Tabla de Cotizaciones");
+            tituloRun.setBold(true); 
+            tituloRun.setFontSize(12); 
+            tituloRun.setFontFamily("Times New Roman"); 
+            tituloRun.setText("COTIZACION");
 
-            // Crear tabla
+            XWPFParagraph encabezado = documento.createParagraph();
+            encabezado.setAlignment(ParagraphAlignment.DISTRIBUTE); 
+            encabezado.setSpacingAfter(200); 
+            XWPFRun encabezadoRun = encabezado.createRun();
+            encabezadoRun.setFontFamily("Times New Roman");
+            encabezadoRun.setFontSize(10); 
+            encabezadoRun.setText("LUGAR Y FECHA: LUGAR, 29 DE MAYO DE 2023.");
+            encabezadoRun.addBreak();
+            encabezadoRun.setText(" ");
+            encabezadoRun.addBreak();
+            encabezadoRun.setText("SEÑORES MIEMBROS DEL CDE.");
+            encabezadoRun.addBreak();
+            encabezadoRun.setText("ESCUELA DE EDUCACIÓN PARVULARIA LUGAR");
+            encabezadoRun.addBreak(); 
+            encabezadoRun.setText("Presente.");
+            encabezadoRun.addBreak();
+            encabezadoRun.setText(" ");
+            encabezadoRun.addBreak();
+            encabezadoRun.setText("Atendiendo la solicitud de cotización de fecha 27 DE Mayo de 2022, suscrita por el CDE que administra La Escuela de Educación Parvularia La LUGAR, ubicado en el Municipio de La LUGAR, atentamente remito la oferta en original apegada a las condiciones y especificaciones de compras detalladas a continuación:\n\n");
+            encabezadoRun.addBreak();
+            encabezadoRun.setText(" ");
+            encabezadoRun.addBreak();
+
             XWPFTable tabla = documento.createTable();
+            tabla.setWidth("100%");
+            tabla.setTableAlignment(TableRowAlign.CENTER);
+
             
-            // Agregar encabezado de la tabla
-            String[] encabezado = {"N", "Cantidad", "Unidad de Medida", "Descripción", "Precio Unitario", "Precio Total"};
-            XWPFTableRow filaEncabezado = tabla.getRow(0); // Obtener la primera fila (encabezado)
-            for (int i = 0; i < encabezado.length; i++) {
+            CTTblPr tblPr = tabla.getCTTbl().addNewTblPr();
+            tblPr.addNewTblBorders().addNewBottom().setVal(STBorder.SINGLE);
+            tblPr.getTblBorders().addNewLeft().setVal(STBorder.SINGLE);
+            tblPr.getTblBorders().addNewRight().setVal(STBorder.SINGLE);
+            tblPr.getTblBorders().addNewTop().setVal(STBorder.SINGLE);
+            tblPr.getTblBorders().addNewInsideH().setVal(STBorder.SINGLE);
+            tblPr.getTblBorders().addNewInsideV().setVal(STBorder.SINGLE);
+
+            
+            String[] encabezadoTabla = {"N", "Cantidad", "Unidad de Medida", "Descripción", "Precio Unitario", "Precio Total"};
+            XWPFTableRow filaEncabezado = tabla.getRow(0); 
+            for (int i = 0; i < encabezadoTabla.length; i++) {
                 XWPFTableCell celda = filaEncabezado.getCell(i);
                 if (celda == null) {
                     celda = filaEncabezado.createCell();
                 }
-                celda.setText(encabezado[i]);
+                XWPFParagraph p = celda.getParagraphs().get(0);
+                p.setAlignment(ParagraphAlignment.DISTRIBUTE); 
+                XWPFRun r = p.createRun();
+                r.setFontSize(10); 
+                r.setFontFamily("Times New Roman"); 
+                r.setBold(true); 
+                r.setText(encabezadoTabla[i]);
             }
 
-            // Agregar filas y celdas a la tabla
+            
             for (Cotizacion cotizacion : listaCotizaciones) {
                 XWPFTableRow fila = tabla.createRow();
                 for (int i = 0; i < 6; i++) {
-                    // Obtener la celda en la posición actual
+                    
                     XWPFTableCell celda = fila.getCell(i);
-                    // Si la celda no existe, crear una nueva
+                    
                     if (celda == null) {
                         celda = fila.createCell();
                     }
-                    // Asignar el texto a la celda
+                   
+                    XWPFParagraph p = celda.getParagraphs().get(0);
+                    p.setAlignment(ParagraphAlignment.DISTRIBUTE); 
+                    XWPFRun r = p.createRun();
+                    r.setFontSize(10); 
+                    r.setFontFamily("Times New Roman");
                     switch (i) {
                         case 0:
-                            celda.setText(String.valueOf(cotizacion.getId_cotizacion()));
+                            r.setText(String.valueOf(cotizacion.getId_cotizacion()));
                             break;
                         case 1:
-                            celda.setText(String.valueOf(cotizacion.getCantidad()));
+                            r.setText(String.valueOf(cotizacion.getCantidad()));
                             break;
                         case 2:
-                            celda.setText(cotizacion.getUnidadMedida());
+                            r.setText(cotizacion.getUnidadMedida());
                             break;
                         case 3:
-                            celda.setText(cotizacion.getDescripcion());
+                            r.setText(cotizacion.getDescripcion());
                             break;
                         case 4:
-                            celda.setText(String.valueOf(cotizacion.getPrecioUnitario()));
+                            r.setText(String.valueOf(cotizacion.getPrecioUnitario()));
                             break;
                         case 5:
-                            celda.setText(String.valueOf(cotizacion.getPrecioTotal()));
+                            r.setText(String.valueOf(cotizacion.getPrecioTotal()));
                             break;
                         default:
                             break;
@@ -78,15 +126,24 @@ public class ExportacionWord {
                 }
             }
 
-            // Escribir documento en archivo
-            FileOutputStream salida = new FileOutputStream(nombreArchivo);
-            documento.write(salida);
-            salida.close();
-            documento.close();
-
-            System.out.println("Tabla exportada correctamente a " + nombreArchivo);
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Guardar como");
+            fileChooser.setFileFilter(new FileNameExtensionFilter("Documento Word (.docx)", "docx"));
+            int seleccion = fileChooser.showSaveDialog(null);
+            if (seleccion == JFileChooser.APPROVE_OPTION) {
+                String rutaArchivo = fileChooser.getSelectedFile().getAbsolutePath() + ".docx";
+                FileOutputStream salida = new FileOutputStream(rutaArchivo);
+                documento.write(salida);
+                salida.close();
+                documento.close();
+                System.out.println("Documento guardado en: " + rutaArchivo);
+            } else {
+                System.out.println("Cancelado por el usuario");
+            }
+            
         } catch (Exception ex) {
             System.err.println("Error al exportar tabla: " + ex.getMessage());
         }
+
     }
 }
