cb2f5aac30e3b5767b4c6225bc658e2aa44958ef
criquez1237 <ing.chenriquez@outlook.com>
2024-05-20 15:22:05 -0600

    feat: implement horizontal page orientation for Word reports and finalize project structure

---
+++ se/streamliningsystem/src/main/java/com/streamliningsystem/models/ViewModels/DetalleOrdenVM.java
@@ -0,0 +1,85 @@
+package com.streamliningsystem.models.ViewModels;
+
+public class DetalleOrdenVM {
+    
+    public int idDetalleOrden;
+    
+    public int numArticulo;
+    
+    public int cantidad;
+    
+    public String unidadMedida;
+    
+    public String descripcionArticulo;
+    
+    public double precioUnitario;
+    
+    public double precioTotal;
+    
+    public int ordenId;
+
+    public int getIdDetalleOrden() {
+        return idDetalleOrden;
+    }
+
+    public void setIdDetalleOrden(int idDetalleOrden) {
+        this.idDetalleOrden = idDetalleOrden;
+    }
+
+    public int getNumArticulo() {
+        return numArticulo;
+    }
+
+    public void setNumArticulo(int numArticulo) {
+        this.numArticulo = numArticulo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getUnidadMedida() {
+        return unidadMedida;
+    }
+
+    public void setUnidadMedida(String unidadMedida) {
+        this.unidadMedida = unidadMedida;
+    }
+
+    public String getDescripcionArticulo() {
+        return descripcionArticulo;
+    }
+
+    public void setDescripcionArticulo(String descripcionArticulo) {
+        this.descripcionArticulo = descripcionArticulo;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public void setPrecioTotal(double precioTotal) {
+        this.precioTotal = precioTotal;
+    }
+
+    public int getOrdenId() {
+        return ordenId;
+    }
+
+    public void setOrdenId(int ordenId) {
+        this.ordenId = ordenId;
+    }
+    
+}
