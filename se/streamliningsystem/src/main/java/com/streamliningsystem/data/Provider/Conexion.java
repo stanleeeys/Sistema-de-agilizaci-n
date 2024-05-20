3210fa7b80765dbd9512e9f1452497ff2f1264f7
criquez1237 <ing.chenriquez@outlook.com>
2024-05-19 21:19:23 -0600

    bug eliminar

---
+++ se/streamliningsystem/src/main/java/com/streamliningsystem/data/Provider/Conexion.java
@@ -0,0 +1,24 @@
+
+package com.streamliningsystem.data.Provider;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexion {
+    
+        Connection con;
+    
+    public Connection getConnection(){
+        try {
+            String myBD= "jdbc:mysql://localhost:3306/sistemaagilizacion_bd?serverTimezone=UTC";
+            con = DriverManager.getConnection(myBD, "nico", ")P4ssword");
+            return con;
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la base de datos: " + e.toString());
+            return null;
+        }
+    }
+}
+
